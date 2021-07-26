67b044ffc1c4086ee80194be86f918024e2c545d
everaldo@bb-8 <juniorklawa10@gmail.com>
2021-07-26 09:31:11 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,88 @@
+const fetch = require("node-fetch");
+
+async function main() {
+  const puppeteer = require("puppeteer");
+  const browser = await puppeteer.launch({
+    headless: false,
+    defaultViewport: null,
+  });
+
+  const Blocker = require("@cliqz/adblocker-puppeteer");
+
+  Blocker.PuppeteerBlocker.fromPrebuiltAdsAndTracking(fetch).then((blocker) => {
+    blocker.enableBlockingInPage(page);
+  });
+
+  const page = await browser.newPage();
+  await page.goto("https://generator.email/");
+
+  const email = await page.evaluate(async () => {
+    function delay(time) {
+      return new Promise(function (resolve) {
+        setTimeout(resolve, time);
+      });
+    }
+    const generatedEmail = document.querySelector("#email_ch_text").textContent;
+    delay(1000);
+    return generatedEmail;
+  });
+
+  const axios = require("axios");
+
+  await axios.post("https://www.geoguessr.com/api/v3/accounts/signup", {
+    email,
+  });
+
+  await page.waitForTimeout(5000);
+
+  await page.click("#refresh > button");
+
+  await page.waitForTimeout(1000);
+
+  const newAccountLinkToken = await page.evaluate(() => {
+    function delay(time) {
+      return new Promise(function (resolve) {
+        setTimeout(resolve, time);
+      });
+    }
+    delay(1000);
+    const button = document.querySelector(
+      "#email-table > div.e7m.row.list-group-item > div.e7m.col-md-12.ma1 > div.e7m.mess_bodiyy > div > div > div:nth-child(3) > a"
+    );
+
+    return button.href;
+  });
+
+  const newAccountPage = await browser.newPage();
+  await newAccountPage.goto(newAccountLinkToken);
+
+  await newAccountPage.waitForSelector(
+    "#__next > div > main > form > section > section.grid__column.grid__column--span-2 > div > div:nth-child(1) > div.form-field__field > input"
+  );
+  await newAccountPage.type(
+    "#__next > div > main > form > section > section.grid__column.grid__column--span-2 > div > div:nth-child(1) > div.form-field__field > input",
+    "123123"
+  );
+
+  await newAccountPage.waitForSelector(
+    "#__next > div > main > form > section > section.grid__column.grid__column--span-2 > div > div:nth-child(2) > div.form-field__field > input"
+  );
+
+  await newAccountPage.type(
+    "#__next > div > main > form > section > section.grid__column.grid__column--span-2 > div > div:nth-child(2) > div.form-field__field > input",
+    "123123"
+  );
+
+  await newAccountPage.waitForTimeout(200);
+
+  await newAccountPage.evaluate(() => {
+    document.querySelectorAll("button")[1].click();
+  });
+
+  await newAccountPage.waitForTimeout(1500);
+  await newAccountPage.click(
+    "#__next > div > div > header > div.header__left > a"
+  );
+}
+
+main();
